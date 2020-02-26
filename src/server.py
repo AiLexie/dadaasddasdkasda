0cdb232d0e6c2ab38db21b3ebbab355c78843822
Daniihh <danielc030417@gmail.com>
2020-02-25 19:53:10 -0500

    Hide MongoDB Credentials

--- src/server.py
+++ src/server.py
@@ -14,7 +14,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 contents: Dict[str, bytes] = dict()
 contents["/"] = open("../site-src/index.html", "r").read().encode("utf-8")
 
-mongo_db = MongoClient("mongodb://localhost")["project-dark"]
+mongo_db = MongoClient(os.getenv("MONGO_DB_CONNECT"))["project-dark"]
 
 class InternalError(Exception):
   message: str
